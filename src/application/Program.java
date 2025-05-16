4c85b8eca8207a55ef16c4c0385c186d1a764b1f
Enzo Camp <enzo.flap@hotmail.com>
2025-05-15 22:45:59 -0300

    Creating DaoFactory and SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,9 +10,9 @@ public class Program {
 
     public static void main(String[] args) {
 
-        Department obj = new Department(1, "Books");
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.now(), 3000.0, obj);
+        Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
     }
